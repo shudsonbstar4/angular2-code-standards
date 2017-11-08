2e03de6f96d8ae1084272bfeb2c91f4c907a3951
shudsonbstar4 <sarah@sarah-hudson.com>
2017-11-07 20:51:58 -0500

    Adds fetch example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Http } from '@angular/http';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+  data: any;
+  errorsExist: Boolean = false;
+  userForm: FormGroup = new FormGroup({ username: new FormControl('')});
+
+  constructor(
+    public toastr: ToastsManager,
+    private http: Http,
+    vcr: ViewContainerRef
+  ) {
+      this.toastr.setRootViewContainerRef(vcr);
+  }
+
+  ngOnInit() {
+    this.userForm.controls.username.valueChanges.subscribe(
+      val => {
+        this.validateInput(val);
+      }
+    )
+  }
+
+  validateInput(val) {
+    this.errorsExist = false;
+    console.log('val', val);
+    if (!val || val > 100 || val === 'undefined') {
+      this.errorsExist = true;
+    }
+  }
+
+  fetch() {
+    let user = this.userForm.controls.username.value;
+    this.validateInput(user);
+
+    if (this.errorsExist) {
+      return;
+    }
+
+    this.http.get(`https://api.github.com/users/${user}`)
+      .subscribe(
+        response => {
+          this.data = response.json();
+          console.log('data', this.data);
+          this.showSuccess();
+        },
+        error => {
+          this.showError();
+        }
+      );
+  }
+
+  showSuccess() {
+    this.toastr.success('Data refreshed!', 'Success!');
+  }
+
+  showError() {
+    this.toastr.error('Could not fetch data!', 'Oops!');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ToastModule } from 'ng2-toastr/ng2-toastr';
 
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     ToastModule.forRoot()
   ],
